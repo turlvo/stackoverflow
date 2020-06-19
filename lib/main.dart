2d4c99405b393ae566863eb370efec69fe354b19
Yeonho Park <yeonho@deepnatural.ai>
2020-06-19 21:36:00 +0900

    resolve 62464471

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animator/animator.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,41 +10,87 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "App",
+      theme: new ThemeData(primarySwatch: Colors.amber),
+      home: Test(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Test extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TestState createState() => _TestState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
+class _TestState extends State<Test> {
+  double rightValue = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Title",
+      theme: new ThemeData(primarySwatch: Colors.amber),
+      home: Scaffold(
+        body: SafeArea(
+          child: Animator(
+              tween: Tween<double>(
+                  begin: 0.0, end: MediaQuery.of(context).size.width),
+              repeats: 0,
+              duration: const Duration(milliseconds: 1000),
+              builder: (context, animatorState, child) {
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                            'assets/bg.png',
+                          ),
+                        ),
+                      ),
+                      height: 200.0,
+                    ),
+                    Positioned(
+                      // left: 0,
+                      left: 70 + animatorState.value,
+                      child: Center(
+                        child: Container(
+                          color: Colors.black.withOpacity(0.5),
+                          width: MediaQuery.of(context).size.width,
+                          height: 200.0,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      // left: 0,
+                      left: animatorState.value,
+                      child: Center(
+                        child: ShaderMask(
+                          shaderCallback: (rect) {
+                            return LinearGradient(
+                              begin: Alignment.centerRight,
+                              end: Alignment.centerLeft,
+                              colors: [Color(0xFF45ced5), Colors.transparent],
+                            ).createShader(
+                                Rect.fromLTRB(0, 0, rect.width, rect.height));
+                          },
+                          blendMode: BlendMode.dstIn,
+                          child: Container(
+                            color: Color(0xFF45ced5),
+                            width: 70.0,
+                            height: 200.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }),
+        ),
       ),
     );
   }
