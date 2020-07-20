ff2d328ab754a74970ca27706dd2cd80053ffe0c
Yeonho Park <yeonho@deepnatural.ai>
2020-07-20 13:18:13 +0900

    i-am-trying-to-fetch-data-from-newsapi-org-but-having-issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,27 +30,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String urlToImage;
+  Future<List<Article>> newslist;
+
+  Future<List<Article>> fetchArticle() async {
+    var url =
+        'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2e8e3846c42a4f64a6b1d98370bdeeea';
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      List<Article> listArticle = data["articles"].map<Article>((article) {
+        return Article.fromJson(article);
+      }).toList();
+      return listArticle;
+    } else {
+      throw Exception('Failed to load Article');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    newslist = fetchArticle();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Api calls',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Api Calls'),
+        ),
+        body: FutureBuilder<List<Article>>(
+          future: newslist,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // return Container();
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return Image.network(snapshot.data[index].urlToImage);
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
 
   Widget _buildBody() {
     return Container();
+  }
+}
+
+class Article {
+  final String author;
+  final String title;
+  final String description;
+  final String publishedAt;
+  final String urlToImage;
+  final String url;
+
+  Article(
+      {this.author,
+      this.description,
+      this.title,
+      this.publishedAt,
+      this.urlToImage,
+      this.url});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+        author: json['author'],
+        title: json['title'],
+        description: json['description'],
+        publishedAt: json['publishedAt'],
+        url: json['url'],
+        urlToImage: json['urlToImage']);
   }
 }
