56d9c07b3564b3416eb687727b0d993d328eb204
Yeonho Park <yeonho@deepnatural.ai>
2020-07-29 14:32:27 +0900

    Scrolling Page to top in SilverChildBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -28,27 +28,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ScrollController _scrollController = ScrollController();
+  bool isEndOfList = false;
+
   @override
   void initState() {
     super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge) {
+        if (_scrollController.position.pixels == 0) {
+          // you are at top position
+          setState(() {
+            isEndOfList = false;
+          });
+        } else {
+          // you are at bottom position
+          setState(() {
+            isEndOfList = true;
+          });
+        }
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: isEndOfList
+          ? FloatingActionButton(
+              onPressed: () {
+                _scrollController
+                  ..animateTo(
+                    0.0,
+                    curve: Curves.easeOut,
+                    duration: const Duration(milliseconds: 300),
+                  );
+              },
+              child: Icon(Icons.keyboard_arrow_up),
+              backgroundColor: Colors.black,
+            )
+          : null,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverAppBar(
+            floating: false,
+            pinned: true,
+            expandedHeight: 255,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                height: 255,
+                width: 380,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/PromotionLabel.png"),
+                      fit: BoxFit.fill),
+                ),
+                child: Transform.translate(
+                  offset: Offset(0, -60),
+                  child: Image.asset(
+                    "images/PromotionLabel.png",
+                    width: 331,
+                    height: 67,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  height: 50,
+                  child: Text('1111'),
+                );
+              },
+              childCount: 20,
+            ),
+          )
+        ],
       ),
     );
-  }
-
-  Widget _buildBody() {
-    return Container();
   }
 }
