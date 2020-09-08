ac47959781a267d64ee51149671998ee38e358ac
Yeonho Park <yeonho@deepnatural.ai>
2020-09-08 16:43:20 +0900

    resolve 63788530

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,50 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return CheckBoxInListView();
+  }
+}
+
+class CheckBoxInListView extends StatefulWidget {
+  @override
+  _CheckBoxInListViewState createState() => _CheckBoxInListViewState();
+}
+
+class _CheckBoxInListViewState extends State<CheckBoxInListView> {
+  List<String> _texts = [
+    "InduceSmile.com",
+    "Flutter.io",
+    "google.com",
+    "youtube.com",
+    "yahoo.com",
+    "gmail.com"
+  ];
+
+  List<bool> _isChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    _isChecked = List<bool>.filled(_texts.length, false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _texts.length,
+      itemBuilder: (context, index) {
+        return CheckboxListTile(
+          title: Text(_texts[index]),
+          value: _isChecked[index],
+          onChanged: (val) {
+            setState(
+              () {
+                _isChecked[index] = val;
+              },
+            );
+          },
+        );
+      },
+    );
   }
 }
