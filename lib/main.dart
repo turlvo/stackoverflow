a31e65426e4d6aefdd5cdcaedae10db8b0bbaf26
Yeonho Park <yeonho@deepnatural.ai>
2020-09-09 15:27:13 +0900

    resolve 63805530

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,20 +36,125 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return Login();
+  }
+}
+
+class Login extends StatelessWidget {
+  Widget _inputField(String title, Color border) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: title,
+        hintStyle: TextStyle(color: border),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: border),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: border),
+        ),
+        border: UnderlineInputBorder(
+          borderSide: BorderSide(color: border),
+        ),
       ),
     );
   }
 
-  Widget _buildBody() {
-    return Container();
+  Widget _buttons(name, BuildContext context) {
+    return Center(
+        child: ButtonBar(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ButtonTheme(
+            minWidth: 200,
+            child: RaisedButton(
+              child: new Text(name),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+                side: BorderSide(color: Colors.white),
+              ),
+              color: Colors.white,
+              textColor: Colors.red,
+              onPressed: () {},
+            )),
+      ],
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(150.0),
+          child: AppBar(
+            flexibleSpace: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 100, vertical: 5),
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: Text('logo'),
+                ),
+              ],
+            ),
+            centerTitle: true,
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+                margin: const EdgeInsets.only(top: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin:
+                          const EdgeInsets.only(left: 25, top: 50, right: 25),
+                      child: _inputField('UserName', Colors.white),
+                    ),
+                    Container(
+                      margin:
+                          const EdgeInsets.only(left: 25, top: 10, right: 25),
+                      child: _inputField('Password', Colors.white),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 25),
+                      child: _buttons('Login', context),
+                    ),
+                  ],
+                ))),
+            Container(
+              margin: const EdgeInsets.only(top: 80),
+              child: Table(
+                border: TableBorder(
+                    top: BorderSide(color: Colors.white, width: 4),
+                    verticalInside: BorderSide(color: Colors.white, width: 4),
+                    horizontalInside:
+                        BorderSide(color: Colors.white, width: 4)),
+                children: [
+                  TableRow(children: [
+                    Icon(Icons.account_circle),
+                    Icon(Icons.access_alarm)
+                  ]),
+                  TableRow(children: [
+                    Icon(Icons.account_circle),
+                    Icon(Icons.access_alarm)
+                  ])
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
