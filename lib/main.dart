aed03704de6e73404c2f041a88103431bd26d390
Yeonho Park <yeonho@deepnatural.ai>
2020-09-09 12:28:20 +0900

    resolve 63799353

--- lib/main.dart
+++ lib/main.dart
@@ -4,51 +4,64 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        backgroundColor: Colors.pink[200],
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 10),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Wedding Organizer',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'Pre-wedding, Photo, Party',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 25.5,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      child: Text('Our services',
+                          style: TextStyle(
+                              fontFamily: 'SourceSansPro',
+                              fontSize: 17,
+                              color: Colors.white)),
+                      color: Colors.red,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Text('345 Moo 1 Tasud Chiang Rai, Thailand',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 17,
+                      color: Colors.grey[300])),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  Widget _buildBody() {
-    return Container();
   }
 }
