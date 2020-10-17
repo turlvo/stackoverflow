c15bafaae19f45871820589978318be239bc7902
Yeonho Park <yeonho@deepnatural.ai>
2020-10-17 22:40:00 +0900

    resolve 64402843

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,74 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return MyForm();
+  }
+}
+
+class MyButton extends StatelessWidget {
+  final formKey;
+  final String label;
+
+  const MyButton({Key key, this.formKey, this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        // I want to be able to retrieve the text via the controllers for the 2 text fields.
+        // currently these values are empty which is the issue.
+        print(
+            'name: ${formKey.currentWidget.child.children[0].controller.text} ');
+        print(
+            'pass: ${formKey.currentWidget.child.children[1].controller.text} ');
+      },
+      child: Text(label),
+    );
+  }
+}
+
+class MyForm extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+
+  final nameController = TextEditingController();
+  final passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          MyField(
+            title: 'name',
+            controller: nameController,
+          ),
+          MyField(
+            title: 'pass',
+            controller: passController,
+          ),
+          MyButton(label: 'Button', formKey: formKey)
+        ],
+      ),
+    );
+  }
+}
+
+class MyField extends StatefulWidget {
+  final String title;
+  final TextEditingController controller;
+
+  const MyField({this.controller, this.title});
+
+  @override
+  _MyFieldState createState() => _MyFieldState();
+}
+
+class _MyFieldState extends State<MyField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+    );
   }
 }
