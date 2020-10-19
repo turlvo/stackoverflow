b0a6ae5b202bb1b5c25a9b9133f38443ec5a17e3
Yeonho Park <yeonho@deepnatural.ai>
2020-10-19 15:57:23 +0900

    resolve 64422268

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,30 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildBody(),
+      body: Stack(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 50,
+                color: Colors.yellow,
+                child: Text(
+                  'header',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(child: Container(color: Colors.green)),
+            ],
+          ),
+          Positioned(
+            child: Align(
+              alignment: Alignment.center,
+              child: _buildButton(),
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
@@ -48,7 +71,31 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildBody() {
-    return Container();
+  Widget _buildButton() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.red,
+      ),
+      child: InkWell(
+        onTap: () {
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => GridViewPage()));
+        },
+        child: Text(
+          'Next',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22.0,
+            fontFamily: 'Work Sans',
+            fontWeight: FontWeight.w500,
+            color: Color(0XFF161F51),
+          ),
+        ),
+      ),
+    );
   }
 }
