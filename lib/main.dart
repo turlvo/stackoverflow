849af87406e37a08e0d8e81819845396ee9f37a1
Yeonho Park <yeonho@deepnatural.ai>
2020-10-19 17:16:13 +0900

    resolve 64423339

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildBody(),
+      body: Center(
+        child: profileButton(),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
@@ -48,7 +50,43 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildBody() {
-    return Container();
+  Widget profileButton() {
+    return Container(
+      width: 360,
+      height: 60,
+      child: SizedBox(
+        child: FlatButton(
+          child: Stack(
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "System",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              Positioned(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    child: Text('icon'),
+                  ),
+                ),
+              ),
+              // Image.asset(
+              //   "assets/system.png",
+              //   height: 55,
+              // )
+            ],
+          ),
+          onPressed: () {},
+          textColor: Colors.black,
+          color: Colors.white,
+          shape: OutlineInputBorder(
+              borderSide: BorderSide(
+                  style: BorderStyle.solid, width: 2, color: Colors.black),
+              borderRadius: new BorderRadius.circular(16)),
+        ),
+      ),
+    );
   }
 }
