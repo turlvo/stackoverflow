c160d66664b05fe66336b275ddd1faec52ac0e07
Yeonho Park <yeonho@deepnatural.ai>
2020-11-06 01:16:40 +0900

    resolve 64699683

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:rich_text_controller/rich_text_controller.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'RichText Controller Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(body: RichTextControllerDemo()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class RichTextControllerDemo extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RichTextControllerDemoState createState() => _RichTextControllerDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _RichTextControllerDemoState extends State<RichTextControllerDemo> {
+// Add a controller
+  RichTextController _controller;
+
   @override
   void initState() {
+    // initialize with your custom regex patterns and styles
+    _controller = RichTextController({
+      //
+      //* Returns every Hashtag with red color
+      //
+      RegExp(r"\B#[a-zA-Z0-9]+\b"): TextStyle(color: Colors.red),
+      //
+      //* Returns every Mention with blue color and bold style.
+      //
+      RegExp(r"\B@[a-zA-Z0-9]+\b"): TextStyle(
+        fontWeight: FontWeight.w800,
+        color: Colors.blue,
+      ),
+      //
+      //* Returns every word after '!' with yellow color and italic style.
+      //
+      RegExp(r"\B![a-zA-Z0-9]+\b"):
+          TextStyle(color: Colors.yellow, fontStyle: FontStyle.italic),
+      // add as many expressions as you need!
+    },
+
+        //! starting v1.1.0
+        // Now you have an onMatch callback that gives you access to a List<String>
+        // which contains all matched strings
+        onMatch: (List<String> matches) {
+      // Do something with matches.
+      //! P.S
+      // as long as you're typing, the controller will keep updating the list.
+    }
+        //!
+        );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  Widget _buildBody() {
-    return Container();
+    // TODO: implement build
+    return Center(
+        child: TextField(
+      controller: _controller,
+    ));
   }
 }
