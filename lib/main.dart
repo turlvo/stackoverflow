b498378bd921cd5dff0cbce45dc80d2282c7b2bd
Yeonho Park <yeonho@deepnatural.ai>
2021-03-18 18:17:02 +0900

    resolve 66687332

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,43 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return Container(
+      height: 190,
+      width: 190,
+      child: Stack(
+        children: <Widget>[
+          Image.network(
+              'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+              width: 190.0,
+              height: 190.0,
+              fit: BoxFit.cover),
+          Center(
+            child: Container(
+              width: 90.0,
+              height: 90.0,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 5.0,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
