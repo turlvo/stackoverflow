e10e8b743b8a7fd290dce25246ec406982574e68
Yeonho Park <yeonho@deepnatural.ai>
2021-05-21 01:18:35 +0900

    resolve 67623883

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  List<Widget> list = [];
+
   @override
   Widget build(BuildContext context) {
+    print('${list.length}');
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: ListView(
+        key: UniqueKey(),
+        children: list,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  Widget _buildBody() {
-    return Container();
+    new Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        list.add(Text('${list.length}', key: Key(list.length.toString())));
+      });
+    });
   }
 }
