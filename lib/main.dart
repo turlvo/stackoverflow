54df443ea76cc448643b35ed6f13baaba5373f53
Yeonho Park <yeonho@deepnatural.ai>
2021-07-13 13:09:10 +0900

    resolve 68322329

--- lib/main.dart
+++ lib/main.dart
@@ -40,15 +40,94 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 
   Widget _buildBody() {
-    return Container();
+    return CalendarUI();
+  }
+}
+
+class CalendarUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    DateTime startDate = DateTime.now().toUtc();
+    DateTime endDate = DateTime.utc(2021, 04, 10);
+    // weekday value is Monday = 0, Tuesday = 1...Sunday = 6
+    int weekday = DateTime.now().weekday - 1;
+
+    getDaysInBetween() {
+      final int difference = startDate.difference(endDate).inDays;
+      print(difference);
+      return difference;
+    }
+
+    final items = List<DateTime>.generate(getDaysInBetween(), (i) {
+      DateTime date = endDate;
+
+      return date.add(Duration(days: i));
+    });
+
+    var newList = items.reversed.toList();
+
+    getDaysInBetween();
+
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            child: GridView.builder(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 7,
+                ),
+                itemCount: items.length + weekday,
+                itemBuilder: (BuildContext context, int key) {
+                  if (key < weekday) {
+                    return Card(
+                      color: Colors.white,
+                    );
+                  } else {
+                    int index = key - weekday;
+                    print(key);
+                    return Card(
+                        color: Colors.amber,
+                        child: Center(
+                          child: Text('${newList[index].day} / index: $index'),
+                        ));
+                  }
+                }),
+          ),
+        ),
+        Container(
+          constraints: BoxConstraints(
+            minHeight: 350,
+          ),
+          child: Column(
+            // crossAxisAlignment: ,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Spacer(flex: 1),
+              Text('Mon'),
+              Spacer(flex: 2),
+              Text('Tue'),
+              Spacer(flex: 2),
+              Text('Wed'),
+              Spacer(flex: 2),
+              Text('Thu'),
+              Spacer(flex: 2),
+              Text('Fri'),
+              Spacer(flex: 2),
+              Text('Sat'),
+              Spacer(flex: 2),
+              Text('Sun'),
+              Spacer(flex: 1),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
