d9444ea2d3315ffe332e10efecae269b19ca9d52
Yeonho Park <yeonho@deepnatural.ai>
2021-07-14 13:30:35 +0900

    resolve 68371712

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _scale;
+  Animation<double> _scaleAnimation;
+
   @override
   void initState() {
+    _scale = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 3000));
+    // _scaleAnimation = _scale
+    //     .drive(CurveTween(curve: Curves.ease))
+    //     .drive(Tween<double>(begin: 0.0, end: 1.0))
+    //     .drive(Tween<double>(begin: 1.0, end: 1.1));
+
+    // _scale.forward();
+
+    _scaleAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.1)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 40.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.1, end: 1.0)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 40.0,
+        ),
+      ],
+    ).animate(_scale);
+    _scale.forward();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scale.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Center(
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Container(height: 100, width: 100, color: Colors.red),
+        ),
       ),
     );
-  }
-
-  Widget _buildBody() {
-    return Container();
   }
 }
