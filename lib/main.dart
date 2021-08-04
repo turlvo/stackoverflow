5cfd2768b62f039a2047ef63d243178e0c2bb957
Yeonho Park <yeonho@deepnatural.ai>
2021-08-04 12:53:26 +0900

    resolve 68640078

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -27,28 +28,224 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
+  }
+
+  double getHeight() {
+    return 800;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            DefaultTabController(
+              length: 3,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, value) {
+                  return [
+                    SliverAppBar(
+                      expandedHeight: 200.0,
+                      flexibleSpace: FlexibleSpaceBar(),
+                    ),
+                    SliverPersistentHeader(
+                      pinned: true,
+                      delegate: _SliverAppBarDelegate(
+                        minHeight: 90,
+                        maxHeight: 90,
+                        child: Container(
+                          height: getHeight() * (1 / 11),
+                          width: double.infinity,
+                          color: Colors.green[200],
+                          child: Center(
+                            child: Text(
+                              "TEXT",
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SliverPersistentHeader(
+                      pinned: true,
+                      delegate: _SliverAppBarDelegate(
+                        minHeight: 90,
+                        maxHeight: 90,
+                        child: Container(
+                          color: Colors.green[200],
+                          child: TabBar(
+                            controller: _tabController,
+                            tabs: [
+                              Tab(
+                                child: Text(
+                                  'TITLE1',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Tab(
+                                child: Text(
+                                  'TITLE2',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                              Tab(
+                                child: Text(
+                                  'TITLE3',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Column(
+                      children: [
+                        // RoundedPicture(),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.pink,
+                          size: 150.0,
+                          semanticLabel:
+                              'Text to announce in accessibility modes',
+                        ),
+                        FittedBox(
+                          child: Text("Hello World",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 40)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 20),
+                                  text: 'Info1:  ',
+                                  children: [
+                                    TextSpan(
+                                      text: "123",
+                                      style: TextStyle(),
+                                    ),
+                                  ]),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 20),
+                                  text: 'Info2:  ',
+                                  children: [
+                                    TextSpan(
+                                      text: "abcd",
+                                      style: TextStyle(),
+                                    ),
+                                  ]),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 20),
+                                  text: 'Info3:  ',
+                                  children: [
+                                    TextSpan(
+                                      text: "xyz",
+                                      style: TextStyle(),
+                                    ),
+                                  ]),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: Text("TITLE2"),
+                    ),
+                    Center(
+                      child: Text("TITLE3"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 90,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: InkWell(
+                onTap: () {},
+                child: Icon(Icons.arrow_back),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
 
-  Widget _buildBody() {
-    return Container();
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
