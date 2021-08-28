1dcb6f81d2990ccba2754aae2980e7a96e97d518
turlvo <yeonho@deepnatural.ai>
2021-08-29 01:56:19 +0900

    resolve 68965718

--- lib/main.dart
+++ lib/main.dart
@@ -39,16 +39,79 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildBody(),
+      body: Container(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          show();
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
     );
   }
 
-  Widget _buildBody() {
-    return Container();
+  void show() {
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        context: context,
+        builder: (context) {
+          return Stack(
+            alignment: AlignmentDirectional.bottomStart,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                margin: EdgeInsets.only(top: 35),
+                color: Colors.transparent,
+                child: Container(
+                  padding: EdgeInsets.only(top: 60),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Alessandro Liparoti',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      Text(
+                        '4 games palyed',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 38,
+                  child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://image.flaticon.com/icons/png/512/147/147144.png'),
+                      radius: 34,
+                      backgroundColor: Colors.white),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
