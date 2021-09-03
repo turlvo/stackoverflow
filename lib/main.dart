e17af76577d417737d65e5716e643c7386c7fdec
turlvo <yeonho@deepnatural.ai>
2021-09-03 21:21:58 +0900

    resolve 69044354

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,8 +34,21 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  bool isValidDate({y = 2021, m1, m2, d1, d2}) {
+    DateFormat format = DateFormat('yyyy/M/d');
+    try {
+      String md = '$y/${m1 * 10 + m2}/${d1 * 10 + d2}';
+      format.parseStrict(md);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(isValidDate(m1: 1, m2: 1, d1: 3, d2: 0));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
