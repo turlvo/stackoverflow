cdf2cc4cddb18c006a6fea3cefb446ae55e37b8e
turlvo <yeonho@deepnatural.ai>
2021-09-14 19:36:38 +0900

    resolve 69175644

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildBody(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: _buildBody(),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
@@ -49,6 +52,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    List images = ['img1', 'img2', 'img3'];
+    List text = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n'
+    ];
+
+    return ListView.separated(
+      itemCount: text.length,
+      shrinkWrap: true,
+      separatorBuilder: (context, index) {
+        if ((index + 1) % 5 == 0) {
+          return Text(images[(index) ~/ 5]);
+        } else {
+          return Container();
+        }
+      },
+      itemBuilder: (context, index) {
+        return Text(text[index]);
+      },
+    );
   }
 }
