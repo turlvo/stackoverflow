ce2cb0b5fcfa6daca25b8942e35e3382f2a0a6d0
turlvo <yeonho@deepnatural.ai>
2021-09-14 17:43:45 +0900

    resolve 69174334

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,41 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return Stack(
+        clipBehavior: Clip.none,
+        alignment: AlignmentDirectional.bottomStart,
+        children: <Widget>[
+          Positioned(
+            right: -135,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.orange[400],
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+              child: Text(
+                'Ends after 1 hour',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Text(
+              '%40 Discount',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ]);
   }
 }
