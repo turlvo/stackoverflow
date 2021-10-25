9ced0c56d62f14d07f060e740f165dc27553a984
turlvo <yeonho@deepnatural.ai>
2021-10-25 13:18:17 +0900

    resolve 69702327

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<bool> clicked = [];
+  var data = [
+    1,
+    2,
+    3,
+  ];
   @override
   void initState() {
     super.initState();
+    clicked = List.filled(data.length, false);
   }
 
   @override
@@ -49,6 +56,98 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return ListView.separated(
+      primary: false,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      separatorBuilder: (context, index) => Divider(
+        color: const Color(0xffcccccc),
+        indent: 10.0,
+        endIndent: 10.0,
+      ),
+      itemCount: data.length,
+      itemBuilder: (context, index) => Padding(
+        padding: EdgeInsets.all(6.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: 60,
+              width: 60,
+              child: RaisedButton(
+                elevation: 0.0,
+                color: Colors.white,
+                textColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xffcccccc), width: 0.5),
+                    borderRadius: BorderRadius.circular(10.0)),
+                onPressed: () {
+                  clicked[index] = !clicked[index];
+                  setState(() {});
+                },
+                child: Text(
+                  data[index].toString(),
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            if (clicked[index]) ...[
+              SizedBox(width: 10),
+              Container(
+                height: 60,
+                width: 60,
+                child: RaisedButton(
+                  elevation: 0.0,
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Color(0xffcccccc), width: 0.5),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  onPressed: () {},
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 60,
+                child: RaisedButton(
+                  elevation: 0.0,
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Color(0xffcccccc), width: 0.5),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  onPressed: () {},
+                  child: Text(
+                    '-',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Container(
+                height: 60,
+                width: 60,
+                child: RaisedButton(
+                  elevation: 0.0,
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Color(0xffcccccc), width: 0.5),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  onPressed: () {},
+                  child: Text(
+                    'T',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ]
+          ],
+        ),
+      ),
+    );
   }
 }
