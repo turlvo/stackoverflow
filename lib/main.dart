18e3244becaa88a482e7a3c2fe7535a4a74bd869
turlvo <yeonho@deepnatural.ai>
2021-10-26 16:26:38 +0900

    resolve 69718348

--- lib/main.dart
+++ lib/main.dart
@@ -8,47 +8,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  bool _started = false;
+  bool _isDisappeared = false;
+  Offset _position = Offset(10, 220);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.orange,
+            ),
+            Positioned(
+              top: _position.dy - 30, // Size of widget to reposition correctly
+              left: _position.dx,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: _isDisappeared ? 0.0 : 1.0,
+                child: Draggable(
+                  child: Icon(Icons.home),
+                  feedback: Icon(Icons.home),
+                  onDragEnd: (details) {
+                    print(details.offset);
+                    setState(() {
+                      if (!_started) _started = true;
+                      _position = details.offset;
+
+                      if (_position.dy - 30 > 400 || _position.dx > 300) {
+                        setState(() {
+                          _isDisappeared = true;
+                        });
+                      }
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-  Widget _buildBody() {
-    return Container();
-  }
 }
+
+class AnimatedOpcaity {}
