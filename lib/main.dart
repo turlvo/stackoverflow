b3f9dcb67ed71877bd63cf24fe6057c5d14136f3
turlvo <yeonho@deepnatural.ai>
2022-03-07 22:38:14 +0900

    resolve 71381242

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,27 +31,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  WebViewController _controller;
+  bool buttonshow = false;
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void scrollToTop() {
+    _controller
+        .evaluateJavascript("window.scrollTo({top: 0, behavior: 'smooth'});");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Flutter WebView'),
       ),
-      body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        },
+        onPageFinished: (String url) async {
+          _controller.evaluateJavascript(
+              "window.onscroll = function () {scrollEventChannel.postMessage(window.scrollY)};");
+        },
+        javascriptChannels: {
+          JavascriptChannel(
+              name: 'scrollEventChannel',
+              onMessageReceived: (JavascriptMessage message) {
+                print('>>>>: ${message.message}');
+
+                int position = int.parse(message.message);
+                if (position == 0) {
+                  setState(() {
+                    buttonshow = false;
+                  });
+                } else if (position > 60) {
+                  setState(() {
+                    buttonshow = true;
+                  });
+                }
+              }),
+        },
+      ),
+      floatingActionButton: Visibility(
+        visible: buttonshow,
+        child: FloatingActionButton(
+          onPressed: () {
+            scrollToTop();
+          },
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.navigation),
+        ),
       ),
     );
-  }
-
-  Widget _buildBody() {
-    return Container();
   }
 }
