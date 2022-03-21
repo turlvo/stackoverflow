1921cda0f213ab9564a0991d3839062b150a2ccd
turlvo <yeonho@deepnatural.ai>
2022-03-21 18:02:29 +0900

    resolve 71554970

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,59 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(25.0),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            context: context,
+            builder: (context) => Container(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Wrap(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      // ... another widget
+                      // ...
+                      // ...
+
+                      // Write review text area
+                      Container(
+                        margin: EdgeInsets.fromLTRB(24, 8, 24, 0),
+                        padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+                        width: double.infinity,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xffF7F7F7),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: TextField(
+                          expands: true,
+                          maxLines: null,
+                          decoration: InputDecoration(
+                            hintText:
+                                'Bagaimana pengalaman Anda secara keseluruhan?',
+                            hintStyle: TextStyle(
+                              color: Color(0xffB2B2B2),
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 2000),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
