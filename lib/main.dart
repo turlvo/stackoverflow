b7d8c51ec315ba999b56231f17535c42b663ebd4
turlvo <yeonho@deepnatural.ai>
2022-04-25 21:55:43 +0900

    resolve 71999127

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,78 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return TasksList(
+      tasks: [
+        {
+          'id': '123',
+          'title': 'title1',
+          'desc': 'desc',
+        },
+        {
+          'id': '333',
+          'title': 'title2',
+          'desc': 'descasdfkjsklfjklasdjfklajsldkfalskdfj',
+        }
+      ],
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+  final List<Map<String, dynamic>> tasks;
+  const TasksList({
+    Key key,
+    @required this.tasks,
+  }) : super(key: key);
+
+  // void _cancelOrDeleteCallBack(BuildContext ctx, Task task) {
+  //   task.isCancelled == false
+  //       ? ctx.read<TodosBloc>().add(CancelTask(task: task))
+  //       : ctx.read<TodosBloc>().add(DeleteTask(task: task));
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: GestureDetector(
+        // behavior: HitTestBehavior.translucent,
+        child: ExpansionPanelList.radio(
+          elevation: 3,
+          children: tasks
+              .map(
+                (task) => ExpansionPanelRadio(
+                  headerBuilder: (context, isOpen) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      children: [
+                        Icon(Icons.favorite),
+                        Expanded(
+                          child: Text(
+                            task['desc'],
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Icon(Icons.add_box),
+                        Icon(Icons.bug_report_rounded),
+                      ],
+                    ),
+                  ),
+                  body: ListTile(
+                    title: SelectableText(
+                      'Task:\n${task['title']}\n\nDescription:\n${task['desc']}',
+                      toolbarOptions: const ToolbarOptions(
+                        copy: true,
+                        selectAll: true,
+                        cut: true,
+                      ),
+                    ),
+                  ),
+                  value: Text(task['id']),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
   }
 }
